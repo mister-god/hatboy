c0a150d15a705298de9f23fecfa01468387bcbb9
mister-god <mistergodgod@gmail.com>
2025-05-03 07:20:05 -0400

    Update start_hatboy.sh

--- start_hatboy.sh
+++ start_hatboy.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
 
+# Display Hatboy ASCII Art
+printf "\e[1;92m        _____  \e[0m\n"
+printf "\e[1;92m       /     \ \e[0m\n"
+printf "\e[1;92m      | () () |\e[0m\n"
+printf "\e[1;92m       \  ^  / \e[0m\n"
+printf "\e[1;92m        |||||  \e[0m\n"
+printf "\e[1;92m        |||||  \e[0m\n"
+printf "\e[1;77m     __/_____\__ \e[0m\n"
+printf "\e[1;77m    |___________| \e[0m\n"
+printf "\e[1;77m     MISTER  X \e[0m\n"
+
+# Start the Flask web server
+echo "Starting the local web server..."
+python3 hatboy_server.py &
+
+# Start Cloudflare Tunnel
+echo "Starting Cloudflare Tunnel..."
+cloudflared tunnel run hatboy
